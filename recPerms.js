e1e0273718dd3d23f55f2611e0b190ec5404bcea
Hack Reactor Students <accounts@hackreactor.com>
2014-06-18 07:41:29 -0700

    convert to use strings

--- recPerms.js
+++ recPerms.js
@@ -1,52 +1,48 @@
-var makeIndent = function(n){ 
-  var acc = []; 
-  for( var i = 0; i < n; i++ ) { 
-    acc.push('-'); 
-  } 
-  acc.push('>'); 
-  return acc.join(""); 
-}; 
-
-var recPerms = function(sofar, rest){ 
-  if( rest.length === 0 ){ 
-    console.log("base case reached:", sofar); 
-  } else { 
-    for( var i = 0; i < rest.length; i++ ){ 
-      var remaining = rest.slice(); 
-      remaining.splice(i,1);  
-      var next = sofar.slice();
-      next.push(rest[i]); 
-      recPerms(next, remaining); 
-    } 
+var makeIndent = function(n){
+  var acc = [];
+  for( var i = 0; i < n; i++ ) {
+    acc.push('-');
   }
-}; 
+  acc.push('>');
+  return acc.join("");
+};
 
-
-var recPerms2 = function(sofar, rest, depth){ 
-  console.log(makeIndent(depth) + " called with sofar:", sofar,"rest:",rest)
-  if( rest.length === 0 ){ 
-    console.log("base case reached", rest); 
-  } else { 
-    for( var i = 0; i < rest.length; i++ ){ 
-      var remaining = rest.slice(); 
-      remaining.splice(i,1);  
-      var next = sofar.slice();
-      next.push(rest[i])
-      recPerms2(next, remaining, depth+1); 
-    } 
+var recPerms = function(sofar, rest){
+  if( rest.length === 0 ){
+    console.log("base case reached:", sofar);
+  } else {
+    for( var i = 0; i < rest.length; i++ ){
+      var remaining = rest.substring(0,1) + rest.substring(i+1);
+      var next = sofar + rest.charAt(i);
+      recPerms(next, remaining);
+    }
   }
-}; 
-     
+};
+
+
+var recPerms2 = function(sofar, rest, depth){
+  console.log(makeIndent(depth) + " called with sofar:", sofar,"rest:",rest);
+  if( rest.length === 0 ){
+    console.log("base case reached", sofar);
+  } else {
+    for( var i = 0; i < rest.length; i++ ){
+      var remaining = rest.substring(0,i) + rest.substring(i+1);
+      var next = sofar + rest.charAt(i);
+      recPerms2(next, remaining, depth+1);
+    }
+  }
+};
+
 var test0 = function(){
-  recPerms([],['a','b','c']); 
-}
+  recPerms('','abc');
+};
 
 //test0();
 
-var test1 = function(){ 
-  recPerms2([],['a','b','c'],0); 
-}; 
+var test1 = function(){
+  recPerms2('','abc',0);
+};
 
-test1(); 
+test1();
 
-  
+
--- recSubs.js
+++ recSubs.js
@@ -1,43 +1,45 @@
-var makeIndent = function(n){ 
-  var acc = []; 
-  for( var i = 0; i < n; i++ ) { 
-    acc.push('-'); 
-  } 
-  acc.push('>'); 
-  return acc.join(""); 
-}; 
-
-var recSubs = function(sofar, rest) { 
-
-  if( rest.length === 0 ) { 
-    console.log(sofar); 
-  } else {
-    var next = sofar.slice();
-    next.push(rest[0]); 
-    recS(next, rest.slice(1)); 
-    recS(sofar.slice(), rest.slice(1)); 
+var makeIndent = function(n){
+  var acc = [];
+  for( var i = 0; i < n; i++ ) {
+    acc.push('-');
   }
-}; 
-
-
-var test = function(){ 
-  recS([], ['a','b','c']); 
-} 
-
-var recSubs2 = function(sofar, rest, depth) { 
-  console.log(makeIndent(depth), "called with sofar: ", sofar, "rest: ", rest);
-  if( rest.length === 0 ) { 
-    console.log("base case reached",sofar); 
-  } else {    
-    var next = sofar.slice();
-    next.push(rest[0]); 
-    recSub2(next, rest.slice(1), depth+1); 
-    recSub2(sofar.slice(), rest.slice(1), depth+1); 
-  }
-}; 
-
-var test2 = function(){ 
-  recSub2([], ['a','b','c'], 0); 
+  acc.push('>');
+  return acc.join("");
 };
 
-test2(); 
+var recSubs = function(sofar, rest) {
+
+  if( rest.length === 0 ) {
+    console.log(sofar);
+  } else {
+    recSubs(sofar + rest.charAt(0), rest.substring(1));
+    recSubs(sofar.substring(), rest.substring(1));
+  }
+};
+
+
+var test = function(){
+  recS('','abc');
+}
+
+var recSubs2 = function(sofar, rest, depth) {
+  console.log(makeIndent(depth), "called with sofar: ", sofar, "rest: ", rest);
+  if( rest.length === 0 ) {
+    console.log("base case reached",sofar);
+  } else {
+    recSubs2(sofar + rest.charAt(0), rest.substring(1), depth+1);
+    recSubs2(sofar.substring(), rest.substring(1), depth+1);
+  }
+};
+
+var test1 = function(){
+  recSubs('', 'abc');
+};
+
+var test2 = function(){
+  recSubs2('','abc', 0);
+};
+
+test1();
+console.log('---');
+test2();
